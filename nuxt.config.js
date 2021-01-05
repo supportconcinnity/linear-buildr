259dc672daa9f61ed64aac08ad541a154662921e
XieHui1989 <hui@liquefy.com>
2021-01-05 11:31:01 +0800

    clear package

--- nuxt.config.js
+++ nuxt.config.js
@@ -119,13 +119,6 @@ module.exports = {
         postcss: {
             // 添加插件名称作为键，参数作为值
             // 使用npm或yarn安装它们
-            plugins: {
-              // 通过传递 false 来禁用插件
-              'postcss-url': false,
-              'postcss-nested': {},
-              'postcss-responsive-type': {},
-              'postcss-hexrgba': {}
-            },
             preset: {
               autoprefixer: true
             }
