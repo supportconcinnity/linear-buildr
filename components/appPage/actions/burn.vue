92586a05484815831e088653027189a07605861e
cruzor <gurungabish16@gmail.com>
2021-12-23 00:51:27 +0545

    fix(DarkTheme): fix burn desktop and mobile dark theme

--- components/appPage/actions/burn.vue
+++ components/appPage/actions/burn.vue
@@ -2196,6 +2196,8 @@ export default {
             width: 100%;
             height: 100%;
             min-height: 550px;
+            display: flex;
+            justify-content: center;
           }
 
           .burnBox {
